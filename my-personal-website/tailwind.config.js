071a3652161c17dac441a6ac5cecd785c5baf3b7
Chen <czha959@aucklanduni.ac.nz>
2021-10-25 18:19:31 +1300

    add side navigation

--- my-personal-website/tailwind.config.js
+++ my-personal-website/tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': '#88BDBC',
+      'secondary': '#ffed4a',
+      'danger': '#e3342f',
+     }),
+    extend: {
+    }
   },
   variants: {
     extend: {},
