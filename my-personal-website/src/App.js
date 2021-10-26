6d736fbafe654c3426cbe1774fe270f35422ece5
Chen <czha959@aucklanduni.ac.nz>
2021-10-26 21:44:18 +1300

    add introduction page
    afd

--- my-personal-website/src/App.js
+++ my-personal-website/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { SideNavigation } from './componenets/side-navigation/sideNavigation';
+import Header from './componenets/header';
+import Introduciton from './componenets/sections/introduction';
 
 function App() {
   return (
     <div className="grid grid-cols-8 bg-primary text-white font-sans">
-      <div className= "col-span-7">
+      <div className= "col-start-1 col-end-8 ml-10 mr-10">
         <div className="min-h-screen" id="section-1"> Hello1</div>
-        <div className="min-h-screen" id="section-2"> Hello2</div>
+        <div className="min-h-screen ml-24" id="section-2"> 
+          <Introduciton></Introduciton>
+        </div>
       </div>
       <div>
         <SideNavigation></SideNavigation>
