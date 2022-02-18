edb369a7b4e1fcaae7d83ac9a8bdc6dc18ca2313
Chen <czha959@aucklanduni.ac.nz>
2022-02-19 12:44:43 +1300

    add profile image

--- my-personal-website/src/componenets/sections/home.js
+++ my-personal-website/src/componenets/sections/home.js
@@ -1,19 +1,24 @@
-
+import profilePhoto from '../../profile.jpg'
 
 const Home = () =>{
-    return (<div class="grid grid-cols-2 gap-4 min-h-screen">
-        <div class="grid grid-rows-3 grid-flow-col gap-4">
-            <div class="row-start-2 row-end-2"> 
-                <h1 class="text-center">Profile Pic</h1>
-            </div>
-        </div>
-        <div class="grid grid-rows-3 grid-flow-col gap-4">
+    return (<div class="grid grid-rows-3 grid-flow-col mr-20">
             <div class="row-start-2 row-end-2">  
-                <h1 class="text-center text-7xl font-medium">Chen Zhao</h1>
-                <h1 class="text-center text-4xl font-medium mt-10">Software Enginer at ASB</h1>
+                <div class="grid grid-col-5 gap-4">
+                    <div class="col-start-3 col-end-3">
+                        <div class="h-60 w-60 float-right mr-5">
+                            <img class="object-cover rounded-full" src={profilePhoto} alt="ProfilePhoto"></img>
+                        </div>  
+                    </div>
+                    <div class="col-start-4 col-end-5">
+                        <h1 class="text-center text-8xl font-medium mt-3">Chen Zhao</h1>
+                        <h1 class="text-center text-5xl font-medium mt-10">Software Enginer at ASB</h1> 
+                    </div>
+                </div>
+                  
+                
             </div>
         </div>
-    </div>)
+    )
 }
 
 export default Home;
