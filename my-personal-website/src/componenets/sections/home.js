dc592106d1247c42989f2b235077f0a0c2056665
Chen <czha959@aucklanduni.ac.nz>
2022-02-19 10:12:58 +1300

    add titles

---
+++ my-personal-website/src/componenets/sections/home.js
@@ -0,0 +1,19 @@
+
+
+const Home = () =>{
+    return (<div class="grid grid-cols-2 gap-4 min-h-screen">
+        <div class="grid grid-rows-3 grid-flow-col gap-4">
+            <div class="row-start-2 row-end-2"> 
+                <h1 class="text-center">Profile Pic</h1>
+            </div>
+        </div>
+        <div class="grid grid-rows-3 grid-flow-col gap-4">
+            <div class="row-start-2 row-end-2">  
+                <h1 class="text-center text-7xl font-medium">Chen Zhao</h1>
+                <h1 class="text-center text-4xl font-medium mt-10">Software Enginer at ASB</h1>
+            </div>
+        </div>
+    </div>)
+}
+
+export default Home;
